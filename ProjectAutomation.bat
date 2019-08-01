fc08a314a4144e5fdf7ecb7802a868887f05f8cf
BURCHILL <jarodburchill@gmail.com>
2019-08-01 12:31:33 -0400

    moving ps to bat

---
+++ ProjectAutomation.bat
@@ -0,0 +1,22 @@
+set projectName="test"
+set localPath="L:/Projects/"
+
+cd /D %localPath%
+mkdir %projectName%
+REM npx create-react-app projectName
+cd %projectName%
+git init
+
+pip install -r %~dp0Requirements.txt
+python %~dp0GitHub.py %localPath% %projectName%    
+
+git add *
+git commit -m "initial commit"
+git push origin master
+
+PAUSE
+
+code .
+npm start
+
+
