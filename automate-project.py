c66bbc8573c13357bf9ffe202f0403e54ea5211e
BURCHILL <jarodburchill@gmail.com>
2019-08-05 12:39:12 -0400

    Added comments

--- automate-project.py
+++ automate-project.py
@@ -38,23 +38,27 @@ username = config.get("DEFAULT", "username")
 password = config.get("DEFAULT", "password")
 
 
+# proccess for blank projects
 def Blank():
     os.mkdir(projectName)
     os.chdir(projectName)
     subprocess.run(f"echo # {repoName} >> README.md", shell=True)
 
 
+# process for react projects
 def React():
     subprocess.run(f"npx create-react-app {projectName}", shell=True)
     os.chdir(projectName)
 
 
+# process for react typescript projects
 def ReactTS():
     subprocess.run(
         f"npx create-react-app {projectName} --typescript", shell=True)
     os.chdir(projectName)
 
 
+# project types dict with values for correct process function
 types = {
     'blank': Blank,
     'react': React,
